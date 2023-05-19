ed446c6bc83c35387f3b3be92fdba4aecd05d8f9
MAC <mac@MACui-MacBookPro.local>
2023-05-19 19:33:19 +0600

    Initial commit

---
+++ src/Country.java
@@ -0,0 +1,10 @@
+public enum Country {
+    Bishkek,
+    Osh,
+    JalaAbad,
+    Naryn,
+    Chuy,
+    Talas,
+    Batken;
+
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Country.Batken+"Ai gul");
+        System.out.println(Country.Chuy+"YNtymak");
+        System.out.println(Country.Bishkek+"+Center city");
+        System.out.println(Country.JalaAbad+"Beatiful");
+        System.out.println(Country.Naryn+"Toolor");
+        System.out.println(Country.Osh+"Sulaiman too");
+        System.out.println(Country.Talas+"Beatiful");
+
+    }
+}
